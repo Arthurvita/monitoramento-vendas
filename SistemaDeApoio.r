3d4128e8ad3f5dab967e27eb0053d0945303485f
arthurvita <arthurvitagiovani@hotmail.com>
2024-09-18 21:26:25 -0300

    verificação de quem bateu a meta

--- SistemaDeApoio.r
+++ SistemaDeApoio.r
@@ -2,3 +2,6 @@
 > vendas <- c(15, 28, 22, 16, 31, 15, 18, 34, 11, 20)
 #Saber se meta foi atingida
 > meta_batida <- vendas >= 20
+#verificação de quem bateu a meta
+> meta_batida
+ [1] FALSE  TRUE  TRUE FALSE  TRUE FALSE FALSE  TRUE FALSE  TRUE
